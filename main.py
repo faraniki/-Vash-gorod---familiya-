b42ba6363af0ba47cec449b1986d0eb138353281
Ya#1 <iumopb@gmail.com>
2023-11-09 19:31:56 +0400

    yellow

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QColor, QPainter
+
+
+sys.excepthook = lambda *a: sys.__excepthook__(*a)
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.initUi()
+
+    def initUi(self):
+        self.drawCycle = False
+        self.drawBtn.clicked.connect(self.draw)
+
+    def draw(self):
+        self.drawCycle = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.drawCycle:
+            qp = QPainter(self)
+            qp.setBrush(QColor("yellow"))
+
+            r = random.randint(10, 100)
+            x = random.randint(r, abs(self.drawLbl.rect().width() - r))
+            y = random.randint(r, abs(self.drawLbl.rect().height() - r))
+            qp.drawEllipse(x, y, r, r)
+
+            qp.end()
+            self.drawCycle = False
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MyWidget()
+    win.show()
+    sys.exit(app.exec_())
